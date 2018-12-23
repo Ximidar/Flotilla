f250c2b62725d7bb96a8f2d1ca95e127689d747d
Ximidar <ximidar@gmail.com>
2018-12-23 14:57:05 -0800

    updated submodules

--- BuildResources/Test/CommonTestTools/CommonTestTools.go
+++ BuildResources/Test/CommonTestTools/CommonTestTools.go
@@ -2,7 +2,7 @@
 * @Author: Ximidar
 * @Date:   2018-12-18 11:44:58
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-12-22 16:06:59
+* @Last Modified time: 2018-12-23 13:45:13
  */
 
 package CommonTestTools
@@ -20,6 +20,6 @@ func CheckErr(t *testing.T, mess string, err error) {
 // CheckEquals will check if two object equal eachother
 func CheckEquals(t *testing.T, object1 interface{}, object2 interface{}) {
 	if object1 != object2 {
-		t.Fatal("Objects do not match: OB1:", object1, "OB2:", object2)
+		t.Fatal("Objects do not match OB1:", object1, "OB2:", object2)
 	}
 }
