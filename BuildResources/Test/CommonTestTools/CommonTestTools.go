fdb19a87c3c20d61a80079761f2ce31fdd1f2712
Ximidar <ximidar@gmail.com>
2018-12-22 22:10:58 -0800

    Updated tests for FlotillaStatus and DataStructures

--- BuildResources/Test/CommonTestTools/CommonTestTools.go
+++ BuildResources/Test/CommonTestTools/CommonTestTools.go
@@ -2,7 +2,7 @@
 * @Author: Ximidar
 * @Date:   2018-12-18 11:44:58
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-12-18 11:46:18
+* @Last Modified time: 2018-12-22 16:06:59
  */
 
 package CommonTestTools
@@ -14,5 +14,12 @@ import "testing"
 func CheckErr(t *testing.T, mess string, err error) {
 	if err != nil {
 		t.Fatalf("Failed Check from %v, Error: %v", mess, err)
+	}
+}
+
+// CheckEquals will check if two object equal eachother
+func CheckEquals(t *testing.T, object1 interface{}, object2 interface{}) {
+	if object1 != object2 {
+		t.Fatal("Objects do not match: OB1:", object1, "OB2:", object2)
 	}
 }
