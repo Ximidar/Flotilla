de2bdcfb3b12fce7d9901c0af1ac19e3caa5266c
Ximidar <ximidar@gmail.com>
2019-01-14 18:12:45 -0800

    added tests for the roll call system as well as a flotilla system test.

--- BuildResources/Test/CommonTestTools/CommonTestTools.go
+++ BuildResources/Test/CommonTestTools/CommonTestTools.go
@@ -2,12 +2,21 @@
 * @Author: Ximidar
 * @Date:   2018-12-18 11:44:58
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-12-23 13:45:13
+* @Last Modified time: 2019-01-13 16:35:54
  */
 
 package CommonTestTools
 
-import "testing"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"testing"
+)
 
 // CheckErr will take in an error and a message to display for any failed
 // errors
@@ -22,4 +31,85 @@ func CheckEquals(t *testing.T, object1 interface{}, object2 interface{}) {
 	if object1 != object2 {
 		t.Fatal("Objects do not match OB1:", object1, "OB2:", object2)
 	}
+}
+
+// MakeProcess will make a proess ready to run. If it returns nil
+// then the process failed to create
+func MakeProcess(prog string, args ...string) *exec.Cmd {
+	cmd := new(exec.Cmd)
+	recoverFunc := func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("Recovered From Error:", err)
+			cmd = nil
+		}
+	}
+	defer recoverFunc()
+
+	fmt.Printf("Calling Command %s %s\n", prog, strings.Join(args, " "))
+	cmd = exec.Command(prog, args...)
+	return cmd
+}
+
+// FindFlotillaBinFolder will attempt to find the bin of a supplied executable name
+// This is for finding executables for flotilla.
+func FindFlotillaBinFolder(progname string) (string, error) {
+
+	// Evaluate the gopath environment variable
+	GoPath := os.Getenv("GOPATH")
+
+	if GoPath == "" {
+		return "", errors.New("No environment variable by that name")
+	}
+
+	// Evaluate Flotilla Path
+	FlotillaRelPath := "/src/github.com/ximidar/Flotilla/"
+
+	FlotillaPath := path.Clean(GoPath + FlotillaRelPath)
+
+	if _, err := os.Stat(FlotillaPath); os.IsNotExist(err) {
+		return "", fmt.Errorf("Flotilla Folder Does not Exist: %v", FlotillaPath)
+	}
+
+	// Find folder for progname
+	FlotillaFolder, err := FindFolder(progname, FlotillaPath)
+	if err != nil {
+		return "", err
+	}
+
+	FFBinPath := path.Clean(FlotillaFolder + "/bin/")
+
+	// Every FlotillaFolder should have a bin folder
+	if _, err := os.Stat(FFBinPath); os.IsNotExist(err) {
+		return "", errors.New("bin Folder does not exist for this Program. Try typing \"make\" in the folder")
+	}
+
+	return FFBinPath, nil
+}
+
+// FindFolder will attempt to find the folder at a specified directory and return it's
+// path
+func FindFolder(folderName string, dir string) (string, error) {
+
+	// try to stat the folder
+	_, err := os.Stat(dir)
+	if err != nil {
+		return "", err
+	}
+	// list directory
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return "", err
+	}
+
+	lowerFolderName := strings.ToLower(folderName)
+	for _, f := range files {
+		lowerDirName := strings.ToLower(f.Name())
+		//fmt.Printf("Comparing %v to %v\n", lowerFolderName, lowerDirName)
+		if lowerFolderName == lowerDirName {
+			return path.Clean(dir + "/" + f.Name()), nil
+		}
+	}
+
+	return "", errors.New("Could not find Folder")
 }
---
+++ BuildResources/Test/FlotillaSystemTests/FlotillaSystem_test.go
@@ -0,0 +1,161 @@
+/*
+* @Author: Ximidar
+* @Date:   2019-01-13 15:38:04
+* @Last Modified by:   Ximidar
+* @Last Modified time: 2019-01-14 18:09:35
+ */
+
+// FlotillaSystemTest is a test package to test multiple nodes together.
+// theres bound to be a bunch of mixed tests in here. (It's going to be messy.)
+package FlotillaSystemTest
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/ximidar/Flotilla/BuildResources/Test/CommonTestTools"
+	FakeSerialDevice "github.com/ximidar/Flotilla/BuildResources/Test/FakeSerialDevice/SerialDevice"
+	"github.com/ximidar/Flotilla/Flotilla_CLI/FlotillaInterface"
+)
+
+// StartTestFlotilla will start a Flotilla instance with the most recently compiled binaries
+// Make sure you actually compile Flotilla before running, This also does not include
+// A NATS instance yet. So you will have to supply that for now
+func StartTestFlotilla() (chan bool, error) {
+
+	fileManagerProc, err := CreateProcess("Flotilla_File_Manager", "FlotillaFileManager")
+	if err != nil {
+		return nil, err
+	}
+
+	flotillaStatusProc, err := CreateProcess("FlotillaStatus", "FlotillaStatus")
+	if err != nil {
+		return nil, err
+	}
+
+	commango, err := CreateProcess("Commango", "Commango")
+
+	if err != nil {
+		return nil, err
+	}
+
+	exitChan := make(chan bool, 10)
+
+	killEverything := func() {
+		fmt.Println("Killing Everything")
+		killerr := syscall.Kill(commango.Process.Pid, syscall.SIGKILL)
+		if killerr != nil {
+			fmt.Println(killerr)
+		}
+		killerr = syscall.Kill(flotillaStatusProc.Process.Pid, syscall.SIGKILL)
+		if killerr != nil {
+			fmt.Println(killerr)
+		}
+		killerr = syscall.Kill(fileManagerProc.Process.Pid, syscall.SIGKILL)
+		if killerr != nil {
+			fmt.Println(killerr)
+		}
+	}
+
+	StartProcs := func() {
+		commango.Start()
+		flotillaStatusProc.Start()
+		fileManagerProc.Start()
+		select {
+		case <-exitChan:
+			killEverything()
+			return
+		}
+	}
+	go StartProcs()
+
+	return exitChan, nil
+
+}
+
+func CreateProcess(FolderName string, ExeName string) (*exec.Cmd, error) {
+	ProgBinFolder, err := CommonTestTools.FindFlotillaBinFolder(FolderName)
+	if err != nil {
+		return nil, err
+	}
+
+	Executable := path.Clean(ProgBinFolder + "/" + ExeName)
+	cmd := CommonTestTools.MakeProcess(Executable, "")
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd, nil
+
+}
+
+func run(serial *FakeSerialDevice.FakeSerial, exitChan chan bool) {
+	var buffer []byte
+	ok := "ok\n"
+	for {
+		select {
+		case buf := <-serial.ReceiveStream:
+			buffer = append(buffer, buf)
+			if buf == 10 { // if we detect a newline
+				fmt.Print(string(buffer))
+				buffer = []byte{}
+
+				okb := []byte(ok)
+				<-time.After(10 * time.Millisecond) // Pretend to process command
+				serial.SendBytes(okb)
+
+			}
+		case <-exitChan:
+			return
+		}
+	}
+}
+
+func TestFlotillaPrinting(t *testing.T) {
+	FI, err := FlotillaInterface.NewFlotillaInterface()
+	CommonTestTools.CheckErr(t, "TestFlotillaSystem", err)
+	exitChan, err := StartTestFlotilla()
+	CommonTestTools.CheckErr(t, "TestFlotillaSystem", err)
+
+	// In case we need to ctrl-c out this will always run
+	sendExitSig := func() {
+		fmt.Println("Sending exit sig")
+		exitChan <- true
+	}
+	defer sendExitSig()
+
+	// Allow time for server startup
+	<-time.After(100 * time.Millisecond)
+
+	// Create a Fake Serial Device
+	serial := FakeSerialDevice.NewFakeSerial()
+	go serial.ReadMaster()
+	go run(serial, exitChan)
+
+	// Connect to the Fake Serial Device
+	err = FI.CommSetConnectionOptions(FakeSerialDevice.SerialName, 115200)
+	CommonTestTools.CheckErr(t, "TestFlotillaSystem", err)
+	err = FI.CommConnect()
+	CommonTestTools.CheckErr(t, "TestFlotillaSystem", err)
+	// send Start
+	serial.SendBytes([]byte("start\n"))
+	<-time.After(100 * time.Millisecond)
+
+	// Select the Benchy
+	Files, err := FI.GetFileStructure()
+	CommonTestTools.CheckErr(t, "TestFlotillaSystem", err)
+	Benchy := Files["root"].Contents["3D_Benchy.gcode"]
+	FI.SelectAndPlayFile(Benchy)
+	serial.SendBytes([]byte("ok\n"))
+
+	// Wait until the benchy has finished playing
+
+	// Exit Flotilla system
+	<-time.After(5 * time.Second)
+
+}
