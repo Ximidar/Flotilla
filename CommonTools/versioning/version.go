7186252b88cbd693a36b4b74c9bb73701c923f6e
Ximidar <ximidar@gmail.com>
2019-02-15 12:05:34 -0800

    Added a CommonTools folder which will hold all common tools the nodes may use.

---
+++ CommonTools/versioning/version.go
@@ -0,0 +1,22 @@
+/*
+* @Author: Ximidar
+* @Date:   2019-02-15 11:04:21
+* @Last Modified by:   Ximidar
+* @Last Modified time: 2019-02-15 11:29:34
+ */
+
+package version
+
+var (
+	// Compile info Vars
+
+	// Version will hold the current Version of the program
+	Version string
+	// CompiledBy will hold the name of the flotilla author AKA Matt Pedler.
+	// If you change this to your name, then you are a jerk
+	CompiledBy string
+	// CompiledDate will hold the date this program was compiled
+	CompiledDate string
+	// CommitHash will hold the commit that this version was built on
+	CommitHash string
+)
