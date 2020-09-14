3d398232f218a9e1ac83fcaedffb5dfeb6bcf960
Ximidar <ximidar@gmail.com>
2020-09-13 19:03:26 -0700

    figured out how to change some color

--- FlotillaWeb/src/views/Status/control/printer_control.vue
+++ FlotillaWeb/src/views/Status/control/printer_control.vue
@@ -1,36 +1,36 @@
 <template>
-    <v-container >
+    <v-container :style="primary_css">
         <v-row align="center" justify="start">
             <v-col cols="2">
                 <v-row align="center" justify="center">
                     <v-col align="center" justify="stretch">
                         <v-btn icon large>
-                            <v-icon>$vuetify.icons.solid_arrow_alt_circle_up</v-icon>
+                            <v-icon v-bind:style="secondary_css">$vuetify.icons.solid_arrow_alt_circle_up</v-icon>
                         </v-btn>
                     </v-col>
                 </v-row>
                 <v-row align="center" justify="space-around">
                     <v-col align="center" justify="center">
                         <v-btn icon large>
-                            <v-icon>$vuetify.icons.solid_arrow_alt_circle_left</v-icon>
+                            <v-icon v-bind:style="secondary_css">$vuetify.icons.solid_arrow_alt_circle_left</v-icon>
                         </v-btn>
                     </v-col>
                     <v-col align="center" justify="center">
                         <v-btn icon large>
-                            <v-icon>$vuetify.icons.solid_store_alt</v-icon>
+                            <v-icon v-bind:style="secondary_css">$vuetify.icons.solid_store_alt</v-icon>
                         </v-btn>
                     </v-col>
                     <v-col align="center" justify="center">
                     
                         <v-btn icon large>
-                            <v-icon>$vuetify.icons.solid_arrow_alt_circle_right</v-icon>
+                            <v-icon v-bind:style="secondary_css">$vuetify.icons.solid_arrow_alt_circle_right</v-icon>
                         </v-btn>
                     </v-col>
                 </v-row>
                 <v-row align="center" justify="center">
                     <v-col align="center" justify="stretch">
                         <v-btn icon large>
-                            <v-icon>$vuetify.icons.solid_arrow_alt_circle_down</v-icon>
+                            <v-icon v-bind:style="secondary_css">$vuetify.icons.solid_arrow_alt_circle_down</v-icon>
                         </v-btn>
                     </v-col>
                 </v-row>
@@ -39,7 +39,7 @@
                 <v-row align="center" justify="center">
                     <v-container>
                         <v-btn icon large>
-                            <v-icon>$vuetify.icons.solid_arrow_alt_circle_up</v-icon>
+                            <v-icon v-bind:style="secondary_css">$vuetify.icons.solid_arrow_alt_circle_up</v-icon>
                         </v-btn>
                     </v-container>
                 </v-row>
@@ -53,7 +53,7 @@
                 <v-row align="center" justify="center">
                     <v-container>
                         <v-btn icon large>
-                            <v-icon>$vuetify.icons.solid_arrow_alt_circle_down</v-icon>
+                            <v-icon v-bind:style="secondary_css">$vuetify.icons.solid_arrow_alt_circle_down</v-icon>
                         </v-btn>
                     </v-container>
                 </v-row>
@@ -61,10 +61,10 @@
         </v-row>
         <v-row>
             <v-col cols="2" align="center" justify="center">
-                <v-card outlined flat height="55px">
-                    <v-chip class="ma-2" color="primary">X</v-chip>
-                    <v-chip class="ma-2" color="primary">Y</v-chip>
-                    <v-chip class="ma-2" color="primary">Z</v-chip>
+                <v-card outlined flat height="55px" color="primary">
+                    <v-chip class="ma-2" color="secondary">X</v-chip>
+                    <v-chip class="ma-2" color="secondary">Y</v-chip>
+                    <v-chip class="ma-2" color="secondary">Z</v-chip>
                 </v-card>
             </v-col>
             <v-col cols="1" align="start" justify="center">
@@ -80,9 +80,21 @@
     </v-container>
 </template>
 
-
-<style scoped>
-.motor-column{
-    flex-direction: column;
+<script>
+export default {
+    name: "PrinterControl",
+    components: {},
+    data(){
+        return{
+            secondary_css: {
+                fill: this.$vuetify.theme.currentTheme.secondary
+            },
+            primary_css: {
+                fill: this.$vuetify.theme.currentTheme.primary,
+                color: this.$vuetify.theme.currentTheme.primary,
+                "background-color": this.$vuetify.theme.currentTheme.primary
+            }
+        }
+    }
 }
-</style>
+</script>
